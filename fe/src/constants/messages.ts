4c9fe7ed2ce472ea0274f5cdffa5f6e90d073682
Tr.Nghia <t.nghia2112279@gmail.com>
2026-06-08 01:22:28 +0700

    chore(config): HH-1 - clean project structure, add constants, and translate content

---
+++ fe/src/constants/messages.ts
@@ -0,0 +1,16 @@
+export const SHARED_MESSAGES = {
+  SUCCESS: {
+    CREATE: 'Data created successfully!',
+    UPDATE: 'Data updated successfully!',
+    DELETE: 'Data deleted successfully!',
+    RESET: 'All data reset successfully!',
+    STATUS_CHANGE: 'Status changed successfully!',
+  },
+  ERROR: {
+    UNKNOWN: 'A system error occurred, please try again later!',
+    NOT_FOUND: 'Requested data not found!',
+    INVALID_INPUT: 'Invalid input data!',
+    FUTURE_DATE: 'Cannot check-in for a future date!',
+    TARGET_EXCEEDED: 'Completed count cannot exceed the daily target!',
+  },
+}
